4cea0817d11f47820b9badde9325cd2017bce0f2
Thanachot W <Thanachot.wongmetin@gmail.com>
2023-04-12 20:10:03 +0700

    Update contact us

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -15,13 +15,66 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      <div className="container mx-auto p-4 md:w-8/12 w-96">
-          <div className="flex justify-center grid grid-cols-1 gap-1 gap-y-4 ">
-            <div className='text-white font-bold text-2xl '>Are you experiencing symptoms of office syndrome without realizing it?</div>
-            <div className='text-white'>Its easy to join the test. You can get it through the our website.</div>
-            <div className='text-white'>You can get it through the our website.</div>
+      <div className="container mx-auto p-4 md:w-10/12 w-96 flex justify-center">
+          <div className='grid grid-cols-1 gap-4'>
+          <div className='flex justify-center'>
+            <h1 className='text-2xl tracking-widest pb-8'>Contact Us</h1>
+            </div>
+          <div className="grid grid-cols-3 gap-24 ">
+            <div className="flex flex-col">
+            <div className='pb-8'>VISIT US</div>
+            <div>500 Terry Francois St.</div>
+            <div>San Francisco, CA 94158</div>
+            <div>123-456-7890</div>
+            </div>  
+          <div className="flex flex-col">
+             <div className='pb-8'>OPENING HOURS</div>
+            <div>Mon - Fri: 7am - 10pm</div>
+            <div>​​Saturday: 8am - 10pm</div>
+            <div>​Sunday: 8am - 11pm</div>
+            </div>  
+             <div className="flex flex-col">
+             <div className='pb-8'>CUSTOMER SERVICE</div>
+                        <div>1-800-000-000</div>
+            <div>123-456-7890</div>
+            <div>info@mysite.com</div>
+            </div>  
+          
           </div>
-        </div>
+         
+          
+    <div className="w-full py-16 bg-white  ">
+        <h2 className="text-2xl pb-8 flex justify-center">FOR ANY QUESTIONS, PLEASE SEND US A MESSAGE</h2>
+        <form className="mx-8 space-y-8">
+            <div>
+                <input type="text"
+                    className="w-full p-2 text-sm border-b-2 border-gray-400 outline-none opacity-50 focus:border-blue-400"
+                    placeholder="Full Name" />
+            </div>
+            <div>
+                <input type="text"
+                    className="w-full p-2 text-sm border-b-2 border-gray-400 outline-none opacity-50 focus:border-blue-400"
+                    placeholder="Your Email" />
+                <span className="text-xs text-red-600">Email field required</span>
+            </div>
+            <div>
+                <input type="text"
+                    className="w-full p-2 text-sm border-b-2 border-gray-400 outline-none opacity-50 focus:border-blue-400"
+                    placeholder="Subject" />
+            </div>
+            <div>
+                <textarea name="message"
+                    className="w-full p-6 text-sm border-b-2 border-gray-400  outline-none opacity-50 focus:border-blue-400"
+                    placeholder="Enter your message"></textarea>
+            </div>
+
+            <button className="block w-full px-2 py-4 mt-2 text-white bg-black ">Submit</button>
+        </form>
+    </div>
+
+          </div>
+          </div>
+        
       </main>
     </>
   )
